9924ed3070b97e0ff058ebcd5a3b8d73659d1bec
DavidePizzoli <davidepizzoli1234@gmail.com>
2020-03-16 11:50:19 +0100

    Add Queries file generator

--- inputConverter.py
+++ inputConverter.py
@@ -1,20 +1,29 @@
+import sys
+import gzip
+
+def zerosAdder(value, n):
+    return ("000000" + str(value))[-n:]
+
 def main():
-  query = sys.argv[1]
-  experiment = sys.argv[2]
-# Operazioni sul file delle queries (Conversione da FASTQ a FASTA)
-
-i = 0
-seq = []
-with open(query) as file:
-  data = file.read()
-  if(i % 4 == 1)
-    seq.append(data)
-print seq
-
-# Operazioni sul file degli EXPERIMENTS (Divisione del file in tanti file (uno per ogni gene)
-# Il nome del file dev'essere il nome del gene
+    # Lettura degli argomenti
+    query = sys.argv[1]
+    experiment = sys.argv[2]
+    
+    # Operazioni sul file delle queries (Conversione da FASTQ a FASTA)
+    
+    data = gzip.open(query, "rt").readlines()
+    seq = [x for counter,x in enumerate(data, 1) if (counter % 4 == 2)]
+    
+    output=open('queries.fa','w')
+    for (i,element) in enumerate(seq,1):
+        output.write('>QUERY_' + zerosAdder(i,5) + '\n')
+        output.write(element)
+    output.close()
+    
+    # Operazioni sul file degli EXPERIMENTS (Divisione del file in tanti file (uno per ogni gene)
+    # Il nome del file dev'essere il nome del gene
 
 
 
 if __name__ == '__main__':
-  main()
+    main()
