98b8538dc3bb1ea944332daf9d4630c47d36ce89
DavidePizzoli <davidepizzoli1234@gmail.com>
2020-03-16 14:25:06 +0100

    Update query converter
    Now the input file is not passed as parameter, but it is passed by standard input. The output now is printed on standard output.

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Cartella di lavoro che conterrà:
 - uno script Python che converte un file di input del programma Shark in un file di input del programma HowDeSBT;
 - uno script Python che converte il file di input del programma HowDeSBT in un file di output del programma Shark.
 
+# usage
 
+[Input FASTQ File] | python queryConverter.py > [Output FASTA File]
---
+++ queryConverter.py
@@ -0,0 +1,20 @@
+import sys
+import gzip
+
+def main():
+    # Lettura degli argomenti
+    query = sys.stdin.readline().rstrip()
+    
+    # Operazioni sul file delle queries (Conversione da FASTQ a FASTA)
+    
+    f = gzip.open(query, "rt")
+    for (i,x) in enumerate(f,1):
+        if(i % 4 == 1):
+            sys.stdout.write(">" + x)
+        else:
+            if(i % 4 == 2):
+                sys.stdout.write(x)
+    f.close();
+    
+if __name__ == '__main__':
+    main()
