e39ade708d96ae2ea512e2bc508061d9eb8cb8fc
DavidePizzoli <davidepizzoli1234@gmail.com>
2020-03-21 11:33:52 +0100

    Update OutputConverter

--- outputConverter.py
+++ outputConverter.py
@@ -2,41 +2,37 @@ import sys
 
 def main():
     
-    try:
-        method = sys.argv[1]
-    except IndexError:
-        method = "SINGLE"
+    method = sys.argv[1].lower() if len(sys.argv) > 1 else 'single'
     
     # Operazioni sul file di output (conversione da .dat a .ssv)
     flag = 0
     prefix = ""
-    st = ""
+    st = []
     old = "-1"
     for (i,x) in enumerate(sys.stdin,1):
         fields = x.split(" ")
         if(x[:1] == "*" and int(fields[1]) > 0): # Leggo l'identificativo del gene
             flag = 1;
-            if(st.count("\n") > 1):
-                if(method.lower() == "multiple"):
-                    sys.stdout.write(st)
-            else:
-                sys.stdout.write(st)
-            
+            if((len(st) > 1 and method.lower() == "multiple") or len(st) == 1):
+                for x in st:
+                    sys.stdout.write(prefix + x + "\n")
             prefix = fields[0].rstrip()[1:] + " "
-            st = ""
+            st = []
             cons = 1
         else:
             if(len(fields) == 3):
                 if(flag == 1):
-                    st += prefix + fields[0] + "\n"
+                    st.append(fields[0])
                     flag = 0
                 else:
                     if(fields[2].rstrip() == old and cons == 1):
-                        st += prefix + fields[0] + "\n"
+                        st.append(fields[0])
                     else:
                         cons = 0
                 old = fields[2].rstrip()
-                
+    if((len(st) > 1 and method.lower() == "multiple") or len(st) == 1):
+        for x in st:
+            sys.stdout.write(prefix + x + "\n")        
     
 if __name__ == '__main__':
     main()
